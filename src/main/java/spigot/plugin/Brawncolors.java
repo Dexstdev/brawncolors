be393ca5967fe0f06c2dd1a121fdc5e976ff9ba7
Cristian Daniel <hernandezcardona35@gmail.com>
2025-04-12 00:24:01 +0000

    spigotmc

--- src/main/java/spigot/plugin/Brawncolors.java
+++ src/main/java/spigot/plugin/Brawncolors.java
@@ -1,5 +1,18 @@
 package spigot.plugin;
 
-public class Brawncolors {
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Brawncolors extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage("§aBrawncolors plugin has enbled!");
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage("§cBrawncolors plugin has disabled!");
+    }
 
 }
